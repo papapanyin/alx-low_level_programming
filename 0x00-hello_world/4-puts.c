feabd689c6d7bc96690e93d8206f6533331353fe
Enimil Ashon <ashon.enimil2@gmail.com>
2022-03-13 08:59:33 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ *   * main - Prints a text
+ *
+ *     *
+ *
+ *       * Return: Always 0 (Success)
+ *
+ *         */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+}
